5556f059dd474e245ed769395412c3cd0b116a36
andresperezpayeras <andresp@ati.es>
2014-11-01 23:25:58 +0100

    1

---
+++ MongoDB_UNiversity/m101js/modul1/helloworld/app/http_server_helloWorld.js
@@ -0,0 +1,17 @@
+var express=require("express"),
+cons = require ("consolidate"),
+mongodb = require("mongodb");
+
+console.log("hello world start");
+
+var http = require("http");
+var server=http.createServer(function(request,response){
+	response.writeHead(200,{"Content-Type": "text/plain"});
+	response.end("Hello world\n");
+});
+
+server.listen(8000);
+
+console.log("Server running in localhost:8000");
+
+
